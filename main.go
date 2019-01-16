2ce4a3fdb396fc34c8129657b74b551af5a9c9b7
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-01-16 18:58:42 +0700

    Find all users work!!

--- main.go
+++ main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email uint
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About Page")
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
 }
 
 func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
-	r.HandleFunc("/about", about)
+	r.HandleFunc("/users", allUsers)
 	fmt.Println("Server starting...")
 	http.ListenAndServe(":3000", r)
 }

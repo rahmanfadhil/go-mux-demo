d015d0aa540d4c508c6f7862d092db994a7f2e50
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-01-16 19:43:00 +0700

    Add create user endpoint

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -13,10 +14,17 @@ import (
 var db *gorm.DB
 var err error
 
+type commonModelFields struct {
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
+
 type User struct {
-	gorm.Model
-	Name  string
-	Email uint
+	commonModelFields
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -31,7 +39,19 @@ func allUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var users []User
 	db.Find(&users)
-	json.NewEncoder(w).Encode(users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	db.Create(&User{Email: user.Email, Name: user.Name})
+	json.NewEncoder(w).Encode(&user)
 }
 
 func main() {
@@ -46,7 +66,8 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
-	r.HandleFunc("/users", allUsers)
+	r.HandleFunc("/users", allUsers).Methods("GET")
+	r.HandleFunc("/users", createUser).Methods("POST")
 	fmt.Println("Server starting...")
 	http.ListenAndServe(":3000", r)
 }
